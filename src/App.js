bfe3ecff140b7d6269108dd496549f20287e6149
Carl Cortright <carl@carlcortright.com>
2022-01-09 21:05:23 -0500

    finish basic site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { jsx, ThemeProvider, Themed } from 'theme-ui'
+import styled from 'styled-components'
+
+import { Flex, Link } from 'rebass'
+
+import theme from './theme'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Header>
+        <Flex sx={{width: '100%', justifyContent: 'center'}}>
+          <Themed.h1>
+            Atlas DAO
+          </Themed.h1>
+        </Flex>
+        <Flex sx={{justifyContent: 'space-between'}}>
+          <Link sx={{
+              margin: '0px 20px'
+            }}>
+            <Themed.a href='https://atlasdao.notion.site/atlasdao/Atlas-DAO-735eff68bd2d4e4d8b2047a5a9d76a86'>
+                📓 Manifesto
+            </Themed.a>
+          </Link>
+
+          <Link sx={{
+              margin: '0px 20px'
+            }}>
+            <Themed.a href='https://discord.gg/DhEkPAQd'>
+              💬 Discord
+            </Themed.a>
+          </Link>
+        </Flex>
+      </Header>
+    </ThemeProvider>
   );
 }
+
+const Header = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+
+  background: linear-gradient(-45deg, #F4FFB0, #B0FFB4, #B0F1FF, #B0CBFF);
+	background-size: 400% 400%;
+	animation: gradient 10s ease infinite;
+
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    25% {
+      background-position: 50% 100%;
+    }
+    50% {
+      background-position: 75% 75%;
+    }
+    75% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+`
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,29 @@
+export default {
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#07c',
+    secondary: '#609',
+  },
+  breakpoints: ['40em', '52em', '64em'],
+  fontSizes: [
+    12, 14, 16, 20, 24, 32, 48, 64, 84
+  ],
+  styles: {
+    h1: {
+      fontSize: [6, 7, 8],
+      fontFamily: 'Arial, system-ui',
+      margin: '10px 10px'
+    },
+    a: {
+      textDecoration: 'none',
+      fontSize: [3, 4, 5],
+      fontFamily: 'Arial, system-ui',
+      color: 'text',
+      ':hover': {
+        cursor: 'pointer',
+        textDecoration: 'underline',
+      },
+    },
+  },
+}
